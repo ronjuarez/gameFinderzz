90888522fadfa238c8424639ba86d594abffb87d
Ron Juarez <ronstewartjuarez@gmail.com>
2020-05-06 18:33:27 -0400

    updated messages seed

--- db/seeds/03_messages.sql
+++ db/seeds/03_messages.sql
@@ -1,10 +1,21 @@
 
-INSERT INTO messages (
-title, text, game_id, shopper_id)
-VALUES (
-'I love Mario Kart', 'this is one of fav to play with the fam could i offer u 20 styll?',  1, 1);
 
-INSERT INTO messages (
-title, text, game_id, shopper_id)
-VALUES (
-'animal farm seems interesting...', 'im offering $10 or turnip equivalent?',  1, 1);
+    INSERT INTO messages (
+    title, text, game_id, shopper_id)
+    VALUES (
+    'Re: Ocarina of Time', 'is the price negotiable', 11, 1);
+
+    INSERT INTO messages (
+    title, text, game_id, shopper_id)
+    VALUES (
+    'Re: GTA V', 'Are you available to meet today?',  12, 2);
+
+    INSERT INTO messages (
+    title, text, game_id, shopper_id)
+    VALUES (
+    'Re: Super Mario Bros', 'is the price negotiable',  10, 3);
+
+    INSERT INTO messages (
+    title, text, game_id, shopper_id)
+    VALUES (
+    'Re: Animal Crossing', 'Are you available to meet today?',  8, 2);
